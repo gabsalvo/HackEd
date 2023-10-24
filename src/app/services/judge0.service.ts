34235cd5df4df1593b5132b2bedb9574186f79fc
Gabriele <g.salvo4@studenti.unipi.it>
2023-10-24 09:51:09 +0200

    Trying to fix Judge0 API

--- src/app/services/judge0.service.ts
+++ src/app/services/judge0.service.ts
@@ -1,90 +1,79 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 import { api_host, api_key, api_url } from 'statuses';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Judge0Service {
-
-  constructor() { }
-
   processing: boolean = false;
-  outputDetails: string = "";
+  outputDetails: any = '';
+
+  maxAttempts = 10;
+
+  constructor() {}
 
   async handleCompile(code: string, customInput: string, languageId: number): Promise<string> {
+    this.processing = true;
     const formData = {
-        language_id: languageId,
-        source_code: btoa(code),
-        stdin: btoa(customInput)
+      language_id: languageId,
+      source_code: btoa(code),
+      stdin: btoa(customInput),
     };
-    console.log("Sending data:", formData);
     const url = `${api_url}?base64_encoded=true&fields=stdout`;
 
-    return fetch(url, {
-        method: 'POST',
+    try {
+      const { data } = await axios.post(url, formData, {
         headers: {
-            'Content-Type': 'application/json',
-            'X-RapidAPI-Host': api_host,
-            'X-RapidAPI-Key': api_key
+          'Content-Type': 'application/json',
+          'X-RapidAPI-Host': api_host,
+          'X-RapidAPI-Key': api_key,
         },
-        body: JSON.stringify(formData)
-        
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("res.data", data);
-        const token = data.token;
-        return this.checkStatus(token);  // ritorna una Promise da checkStatus
-    })
-    .catch(err => {
-        this.processing = false;
-        console.log(err);
-        throw err;  // Rigetta l'errore così il chiamante può gestirlo
-    });
+      });
+
+      console.log('res.data', data);
+      const token = data.token;
+      return await this.checkStatus(token);
+    } catch (err) {
+      this.processing = false;
+      console.error(err);
+      throw err;
+    }
   }
 
-  private maxAttempts = 10; // massimo numero di tentativi di polling
-
-private async checkStatus(token: string, attempt = 1): Promise<string> {
+  private async checkStatus(token: string, attempt = 1): Promise<string> {
     if (attempt > this.maxAttempts) {
-        console.error('Max polling attempts reached. Stopping.');
-        throw new Error('Max polling attempts reached.');
+      this.processing = false;
+      console.error('Max polling attempts reached. Stopping.');
+      throw new Error('Max polling attempts reached.');
     }
 
     const url = `${api_url}/${token}?base64_encoded=true&fields=*`;
 
-    return fetch(url, {
-        method: 'GET',
+    try {
+      const { data } = await axios.get(url, {
         headers: {
-            'X-RapidAPI-Host': api_host,
-            'X-RapidAPI-Key': api_key
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
+          'X-RapidAPI-Host': api_host,
+          'X-RapidAPI-Key': api_key,
+        },
+      });
+
       const statusId = data.status?.id;
       console.log(`Received statusId: ${statusId} on attempt ${attempt}`);
 
       if (statusId === 1 || statusId === 2) {
-          return new Promise<string>((resolve, reject) => {
-              setTimeout(() => {
-                  this.checkStatus(token, attempt + 1).then(output => {
-                      resolve(output);
-                  }, reject);
-              }, 2000);
-          });
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        return await this.checkStatus(token, attempt + 1);
       } else {
-          this.processing = false;
-          this.outputDetails = data;
-          console.log('response.data', data);
-          return typeof data.stdout === 'string' ? atob(data.stdout) : '';
-      }
-  })
-    .catch(err => {
-        console.log("err", err);
         this.processing = false;
-        throw err;
-    });
-}
-
+        this.outputDetails = data;
+        console.log('response.data', data);
+        return typeof data.stdout === 'string' ? atob(data.stdout) : '';
+      }
+    } catch (err) {
+      this.processing = false;
+      console.error(err);
+      throw err;
+    }
+  }
 }
